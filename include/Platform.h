37fea2ac9c7138839ff8bf67a6d5f7b620885c55
Gael <gael.henry.sae@gmail.com>
2019-10-28 17:02:14 +0100

    ajout d'exemple: trap spike

--- include/Platform.h
+++ include/Platform.h
@@ -14,20 +14,27 @@ private:
 	sf::Sprite platformSprite_;
 	sf::Texture platformTextureBig_;
 	sf::Sprite platformSpriteBig_;
+	/*sf::Texture platformSpriteSpike_;
+	sf::Sprite platformSpriteSpike_;*/
 	std::vector<sf::Sprite> platformsSprite1;
 	std::vector<sf::Sprite> platformsSprite2;
 	std::vector<sf::Sprite> platformsSprite3;
+	std::vector<sf::Sprite> platformsSprite4;
 	b2Body* platformBody1_ = nullptr;
 	sf::Vector2f platform1 = sf::Vector2f(100, 225);
 	b2Body* platformBody2_ = nullptr;
 	sf::Vector2f platform2 = sf::Vector2f(680, 275);
 	b2Body* platformBody3_ = nullptr;
 	sf::Vector2f platform3 = sf::Vector2f(290, 425);
+	b2Body* platformBody4_ = nullptr;
+	sf::Vector2f platform4 = sf::Vector2f(295, 395);
 	sf::RectangleShape boxPlatform1RectDebug_;
 	sf::RectangleShape boxPlatform2RectDebug_;
 	sf::RectangleShape boxPlatform3RectDebug_;
+	sf::RectangleShape boxPlatform4RectDebug_;
 	const sf::Vector2f boxSize1 = sf::Vector2f(150, 30);
 	const sf::Vector2f boxSize2 = sf::Vector2f(150, 30);
 	const sf::Vector2f boxSize3 = sf::Vector2f(270, 30);
+	const sf::Vector2f boxSize4 = sf::Vector2f(40, 20);
 	
 };
